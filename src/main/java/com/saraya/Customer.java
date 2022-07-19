9ef9b3fc3b42a6e3e31e3086ee4fe4a2214817b3
ousmaneG <gassamaousmane@gmail.com>
2022-07-19 17:50:58 +0000

    first commit

---
+++ src/main/java/com/saraya/Customer.java
@@ -0,0 +1,26 @@
+package com.saraya;
+
+
+
+public class Customer {
+	
+	private final long id;
+	private final String content;
+	
+	
+	public Customer(long id, String content) {
+		
+		this.id = id;
+		this.content = content;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+
+}
